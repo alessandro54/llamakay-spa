e5cbf9ae139f9fe4320ac66decdf88b98f7ee18f
Alessandro Chumpitaz <par_214@hotmail.com>
2021-06-29 18:45:55 -0500

    Added Profile Page with async JS

--- src/components/Router.svelte
+++ src/components/Router.svelte
@@ -3,6 +3,7 @@
   import Home from "../pages/Home.svelte";
   import Login from "../pages/Login.svelte";
   import Dashboard from "../pages/Dashboard.svelte"
+  import Profile from "../pages/Profile.svelte"
 </script>
 
 
@@ -11,6 +12,7 @@
   <Route path="/" component={Home}/>
   <Route path="/dashboard" component={Dashboard}/>
   <Route path="login" component={Login}/>
+  <Route path="profile" component={Profile}/>
 </Router>
 
 
---
+++ src/pages/Profile.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from "svelte";
+  import Layout from "../components/layout/Layout.svelte";
+  import {getProfile} from "../requests/profile";
+
+  const promise = getProfile('fe0e1751f0bcf14c7c1c4fb3336e8220');
+</script>
+
+<Layout>
+  <div>
+    <h1>My Profile</h1>
+    <div>
+      {#await promise}
+        {:then { first_name, last_name, email, bio, experience, education }}
+          <h1>{first_name} {last_name}</h1>
+          <span>{email}</span>
+          <div>
+            <p>Personal Description: {bio}</p>
+            <p>Professional Experience: {experience}</p>
+            <p>Education: {education}</p>
+          </div>
+        {:catch error}
+      {/await}
+    </div>
+  </div>
+</Layout>
